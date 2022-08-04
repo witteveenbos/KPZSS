b25098b0dc0436079a23a9dcdda8255942fbc761
Daan Bader <daan.bader@witteveenbos.com>
2022-08-04 15:33:58 +0200

    folder aangemaakt, hydra sommen allemaal verwijderen script en hydra profielen schrijven scripts

---
+++ hydra-nl/delete_hydra_calculations.py
@@ -0,0 +1,34 @@
+import os, shutil
+
+
+hydra_werkmap = r'C:\MyPrograms\Hydra-NL_KP_ZSS\werkmap'
+
+for folder in os.listdir(hydra_werkmap):
+
+    # geïnteresseerd in de data van generieke database-namen
+    string_to_match = ["Westerschelde", "Waddenzee"]
+
+    for substring in string_to_match:
+        if substring in folder:
+            folder_path = os.path.join(hydra_werkmap, folder)
+            for map in os.listdir(folder_path):
+                if 'Shapes' in map:
+                    print('shapes doen we niet')
+                elif os.path.isdir(os.path.join(hydra_werkmap, folder, map)):
+                    shutil.rmtree(os.path.join(hydra_werkmap, folder, map))
+                # print(map)
+                # if 'Shapes' in os.path.join(hydra_werkmap, folder, map):
+                #     print('Shape file doen we niet')
+                # elif os.path.isdir(os.path.join(hydra_werkmap, folder, map)):
+                #     print(map)
+
+
+
+# for filename in os.listdir(folder):
+#     file_path = os.path.join(folder, filename)
+#     try:
+#         if os.path.isfile(file_path) or os.path.islink(file_path):
+#             os.unlink(file_path)
+#         elif os.path.isdir(file_path):
+#             shutil.rmtree(file_path)
+#     except Exception as e:
---
+++ hydra-nl/hydrasommen.py
@@ -0,0 +1,68 @@
+ # -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 08:32:24 2020
+
+@author: BADD
+"""
+
+
+from pathlib import Path
+import csv
+import os
+import pandas
+import numpy as np
+
+
+# Set paths
+profile_dir = Path(r'C:\MyPrograms\Hydra-NL_KP_ZSS\profielen')
+os.chdir(profile_dir)
+data_profielen = pandas.read_csv(r'D:\Users\BADD\Desktop\KP ZSS\OKADER_FC_Hydra_attributes.csv')
+
+# strip taludhelling van bijvoorbeeld: 1op5 naar 5
+data_profielen['FC_Tld'] = data_profielen['FC_Tld'].str[-1:]
+
+count = 0
+
+# loop over gehele csv om profielen te schrijven    
+for i in range(0,len(data_profielen)):
+    count += 1
+ 
+    
+    file = open(f"%s.prfl" %data_profielen['VakId'].loc[i],"w")
+    txt=['VERSIE    4.0',
+         'ID'  f'   %s' %data_profielen['VakId'].loc[i],
+         '',
+          f'RICHTING	%.2f'  %data_profielen['FC_DN'].loc[i],
+         '',
+         'DAM   0',
+         'DAMHOOGTE     0',
+         '',
+         'VOORLAND     0',  
+         '',    
+         'DAMWAND  0',           
+
+          f'KRUINHOOGTE  %.2f'   %data_profielen['FC_KH'].loc[i],
+
+
+          'DIJK     2',
+
+          f'-%.2s0.000  0.000 1.000' %data_profielen['FC_Tld'].loc[i],
+          '0.000	10.000	1.000',
+          '', 
+          'MEMO', 
+          '', 
+          '']
+    file.write('\n'.join(txt))    
+    file.close()
+
+    del file, txt    
+        
+
+# loop nogmaals om csv te schrijven waar hydra de info uit haalt
+filecsv = r"D:\Users\BADD\Desktop\KP ZSS\profielen_hydra.csv"
+with open(filecsv, 'w') as f:
+    f.write("Locatie;X-coordinaat;Y-coordinaat;Profiel;Directory\n")
+
+    for i in range(0,len(data_profielen)):
+        f.write(f"{data_profielen['Name'].loc[i]};{data_profielen['XCoordinat'].loc[i]};{data_profielen['YCoordinat'].loc[i]};{data_profielen['VakId'].loc[i]};")
+        f.write("..\\profielen\n")
