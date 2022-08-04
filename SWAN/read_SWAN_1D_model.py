0edf97796e082c9ae2838a4a33593b86278927b2
BEMC <cas.van.bemmelen@witteveenbos.com>
2022-08-04 17:58:10 +0200

    loop over all test calculations

--- SWAN/read_SWAN_1D_model.py
+++ SWAN/read_SWAN_1D_model.py
@@ -8,40 +8,48 @@ Created on Thu Aug  4 14:59:48 2022
 import matplotlib.pyplot as plt
 import numpy as np
 from hmtoolbox.WB_SWAN import SWAN_read_tab
+from hmtoolbox.WB_basic import list_files_folders
+from hmtoolbox.WB_basic import save_plot
 
-path = 'z:\\130991_Systeemanalyse_ZSS\\3.Models\\SWAN\\1D\\tests\\'
-data, headers = SWAN_read_tab.Freadtab(path+'uitvoer.txt')
+path = 'z:\\130991_Systeemanalyse_ZSS\\3.Models\\SWAN\\1D\\Westerschelde\\tests\\test_02\\'
+output_path = 'z:\\130991_Systeemanalyse_ZSS\\5.Results\\SWAN\\1D\\WS\\test_02\\'
+files = list_files_folders.list_files('uitvoer.txt',path)
 
-data['Hsig'][data['Hsig']<=0] = np.nan
+for file in files:
+    
+    data, headers = SWAN_read_tab.Freadtab(file)
+    
+    data['Hsig'][data['Hsig']<=0] = np.nan
+    data['Tm_10'][data['Tm_10']<=0] = np.nan
+    
+    #%% plotting
+    fig = plt.figure(figsize=(6,7))
+    ax1 = plt.subplot(2,1,1)
+    ax1_copy = ax1.twinx()
+    
+    ax1.plot(data['Xp'],data['Botlev'],'k',label = 'bathymetrie')
+    ax1.plot(data['Xp'], data['Watlev'], 'b--', label = 'waterstand')
+    ax1.set_ylabel('hoogte [m+NAP]')
+    
+    ax1_copy.plot(data['Xp'], data['Hsig'],'g')#, label = '$H_s$ [m]')
+    ax1_copy.set_ylabel('$H_s$ [m]',color='g')
+    ax1_copy.tick_params(labelcolor='g')
+    plt.xlabel('afstand [m]')
+    #plt.legend()
+    plt.title(file.split('\\')[-2])
+    
+    ax2 = plt.subplot(2,1,2)
+    ax2_copy = ax2.twinx()
+    ax2.plot(data['Xp'],data['Botlev'],'k',label = 'bathymetrie')
+    ax2.plot(data['Xp'], data['Watlev'], 'b--', label = 'waterstand')
+    ax2.set_ylabel('hoogte [m+NAP]')
+    
+    ax2_copy.plot(data['Xp'], data['Tm_10'],color='orange')
+    ax2_copy.set_ylabel('$H_s$ [m]',color='orange')
+    ax2_copy.tick_params(labelcolor='orange')
+    ax2_copy.set_ylabel('$T_{m-1,0}$ [s]')
 
-data['Tm_10'][data['Tm_10']<=0] = np.nan
-
-#%% plotting
-plt.figure(figsize=(6,7))
-ax1 = plt.subplot(2,1,1)
-ax1_copy = ax1.twinx()
-
-ax1.plot(data['Xp'],data['Botlev'],'k',label = 'bathymetrie')
-ax1.plot(data['Xp'], data['Watlev'], 'b--', label = 'waterstand')
-ax1.set_ylabel('hoogte [m+NAP]')
-
-ax1_copy.plot(data['Xp'], data['Hsig'],'g')#, label = '$H_s$ [m]')
-ax1_copy.set_ylabel('$H_s$ [m]',color='g')
-ax1_copy.tick_params(labelcolor='g')
-plt.xlabel('afstand [m]')
-#plt.legend()
-plt.title(path+'uitvoer.txt')
-
-ax2 = plt.subplot(2,1,2)
-ax2_copy = ax2.twinx()
-ax2.plot(data['Xp'],data['Botlev'],'k',label = 'bathymetrie')
-ax2.plot(data['Xp'], data['Watlev'], 'b--', label = 'waterstand')
-ax2.set_ylabel('hoogte [m+NAP]')
-
-ax2_copy.plot(data['Xp'], data['Tm_10'],color='orange')
-ax2_copy.set_ylabel('$H_s$ [m]',color='orange')
-ax2_copy.tick_params(labelcolor='orange')
-ax2_copy.set_ylabel('$T_{m-1,0}$ [s]')
-
+    save_name = output_path+file.split('\\')[-2]
+    save_plot.save_plot(fig,save_name)
 
 
